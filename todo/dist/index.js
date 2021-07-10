fdec8e4ede9ba46213da88ba3accf7496d015850
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-07-10 19:13:14 +0500

    Step 05:- Test for new item
    1:- Steps
    	a:- updated "src\index.ts" to define a new task items
    	b:- "tsc" to genrate
    		i:- dist\index.js
    	c:- "node dist/index.js" to check output

--- todo/dist/index.js
+++ todo/dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Item_1 = require("./Item");
-let item = new Item_1.Item(1, "First Task", true);
-item.printTask();
+let item1 = new Item_1.Item(1, "First Task", true);
+let item2 = new Item_1.Item(1, "Second Task", false);
+let item3 = new Item_1.Item(1, "Third Task");
+item1.printTask();
+item2.printTask();
+item3.printTask();
--- todo/src/index.ts
+++ todo/src/index.ts
@@ -1,4 +1,9 @@
 import { Item } from "./Item";
 
-let item: Item = new Item(1, "First Task", true);
-item.printTask();
+let item1: Item = new Item(1, "First Task", true);
+let item2: Item = new Item(1, "Second Task", false);
+let item3: Item = new Item(1, "Third Task");
+
+item1.printTask();
+item2.printTask();
+item3.printTask();
